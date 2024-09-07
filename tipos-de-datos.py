b159bf27fd0637b11d78004c83030b152ccb38cb
JamesJojoa1 <jamesai.jaj@gmail.com>
2024-09-06 20:48:52 -0500

    abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,17 @@
+"string" #string se utiliza para cadenas de texto
+'string' #así también podemos escribir un string
+
+
+
+"""Tus datos son:
+    nombre: James
+    Apellido: Jojoa"""
+'''Tus datos son:
+    nombre: James
+    Apellido: Jojoa'''
+#En python tenemos diferentes tipos de números
+4 #número entero
+40.2 #número tipo float
+#También tenemos el tipo de datos boolean
+True #verdadero
+False #falso
