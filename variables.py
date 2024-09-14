34567abb38cc475aef5974b2d47e0eaf2aabeaf0
JamesJojoa1 <jamesai.jaj@gmail.com>
2024-09-13 19:05:47 -0500

    se adordo el tema de variables

--- variables.py
+++ variables.py
@@ -6,3 +6,6 @@ nombre = "James"
 nombre = "Alexander"
 #Podemos concluir que las variables son modificables
 print(nombre)
+
+numero = 10 #primero ejecuta esta linea de código y verás que se imprime el 10
+numero =10+1 #luego ejecuto esta linea de código y verás que se imprime el 11
