138c615d1019b592e3e64f0f1d31835d398685b6
JamesJojoa1 <jamesai.jaj@gmail.com>
2024-09-21 12:20:51 -0500

    abordamos operadores aritmeticos

---
+++ operaciones_aritmeticas.py
@@ -0,0 +1,15 @@
+#suma y resta (+ y -)
+suma = 12 + 4
+resta = 12 -4
+#multiplicación y división (* y -)
+multiplicación = 12 * 4
+división = 12 / 4 # devuelve a float
+#potenciación (**) (exponente)
+potenciación =12 ** 4
+#división entera (//)
+división_entera = 12 // 4
+#resto (%) (modulo)
+resto = 12 % 4
+print(suma, resta, multiplicación, división, potenciación, división_entera,resto)
+tipo_de_dato = type(división_entera) # devuelve el tipo de dato
+print(tipo_de_dato)
